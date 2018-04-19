e5941bab43637347dc81cb93cc276feb50384eb0
cindych419@gmail.com <cindych419@gmail.com>
2018-04-19 00:06:47 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, v: #{colors[6]}"
+def display_rainbow(colors_l)
+  puts "R: #{colors_l[0]}, O: #{colors_l[1]}, Y: #{colors_l[2]}, G: #{colors_l[3]}, B: #{colors_l[4]}, I: #{colors_l[5]}, v: #{colors_l[6]}"
 end
